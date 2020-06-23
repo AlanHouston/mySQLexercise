a4c7a97b992c6adfd2aa5320a7b61fc30c4fff56
Alan Houston <alanhouston5000@gmail.com>
2020-06-23 08:53:51 -0500

    id

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.testHost,
     user: 'altesting',
     password: process.env.testPass,
@@ -19,16 +19,48 @@ connection.connect(function(error) {
     }
 });
 
-app.get('/', function(req, res) {
-    connection.query('SELECT * FROM sampletable', function(error, rows, fields) {
-        if(!!error) {
-            console.log('Error ' + error);
+app.get('/', (req,res) => {
+    res.send('Try using a pathname!');
+});
+
+app.get('/crew/:id', (req,res) => {
+        const userId = req.params.id;
+        const querystring = 'SELECT * FROM sampletable WHERE id = ?';
+        connection.query(querystring, [userId], (err, rows, fields) => {
+            if(!!err) {
+                console.log(err);
+                res.end();
+            } else {
+                res.json(rows);
+            }
+        });
+})
+
+app.get('/numerical', (req,res) => {
+    connection.query('SELECT * FROM sampletable ORDER BY id', function(err, rows, fields) {
+        if(!!err) {
+            console.log(err)
+        } else {
+            res.send(rows);
+        }
+    });
+});
+
+app.get('/crew', function(req, res) {
+    connection.query('SELECT * FROM sampletable', function(err, rows, fields) {
+        if(!!err) {
+            console.log('Error ' + err);
         } else {
             // console.log(rows[0].id + ' ' + rows[0].name);
             // console.log(rows[1].id + ' ' + rows[1].name);
-            for (let i=0; i < rows.length; i++) {
-                console.log(rows[i].id + ' ' + rows[i].name);
-            }
+            
+            // let people = [];
+            // for (let i=0; i < rows.length; i++) {
+            //     let fullName = rows[i].id + ' ' + rows[i].name;
+            //     people.push(fullName);
+            // }
+            // res.send(people);
+            res.json(rows);
         }
     });
 });
